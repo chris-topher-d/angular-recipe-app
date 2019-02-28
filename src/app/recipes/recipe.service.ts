6fbaa59ff3c526a470abb00c8519418374798d0f
Christopher Dennis <cdennis.aus@gmail.com>
2019-02-28 16:54:24 -0600

    Added new default recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,28 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Recipe 2', 'This is test recipe 2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Big Rice Bowl',
+            'An absolutely massive bowl of rice',
+            'https://upload.wikimedia.org/wikipedia/commons/7/77/Steamed_rice_in_bowl_01.jpg',
+            [
+                new Ingredient('Bag of rice', 1)
+            ]
+        ),
+        new Recipe(
+            'Spaghetti',
+            'Old school spaghetti recipe',
+            'https://www-tc.pbs.org/food/wp-content/blogs.dir/2/files/2015/04/hepburn-spaghetti-pomodoro640x360.jpg',
+            [
+                new Ingredient('Spaghetti noodles, 16 oz bag', 1),
+                new Ingredient('Spaghetti sauce, large jar', 1),
+                new Ingredient('Large can of peeled Roma tomatoes', 1)
+            ]
+        )
     ];
 
     getRecipes() {
