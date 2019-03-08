168b4b3722a6db97acf07d3c4ae888b47df413aa
Christopher Dennis <cdennis.aus@gmail.com>
2019-03-07 23:23:51 -0600

    Created path for :id/edit using RecipeEditComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
     { path: ':id', component: RecipeDetailComponent },
-    { path: 'new', component: RecipeEditComponent }
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
