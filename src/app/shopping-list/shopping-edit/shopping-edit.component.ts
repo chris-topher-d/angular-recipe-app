99d2cc24ae234be0484e4eab6a39452a39a2aa3f
Christopher Dennis <cdennis.aus@gmail.com>
2019-03-18 21:13:40 -0500

    Added if/else to onAddIngredient to discern if ingredient is new, or being updated

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,8 +34,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   onAddIngredient(form: NgForm) {
     const value = form.value;
-    const ingredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(ingredient);
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy() {
